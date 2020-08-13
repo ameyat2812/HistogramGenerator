3f8c7f1acd4b122762d809c2bd84df23b3a2b934
Ameya Tambekar <31833755+ameyat2812@users.noreply.github.com>
2020-08-13 15:53:08 +1000

    Update install-dependancies.sh

--- install-dependancies.sh
+++ install-dependancies.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
-sudo apt-get update -y
-sudo apt-get install -y g++ build-essential cmake clang-tools clang-format libgtest-dev libpng-dev
+apt-get update -y
+apt-get install -y g++ build-essential cmake clang-tools clang-format libgtest-dev libpng-dev
